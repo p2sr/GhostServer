277177632c3f59d42d95676b59a3f8543fa23af9
mlugg <mlugg@mlugg.co.uk>
2021-02-08 19:01:54 +0000

    Fix UB caused by removing elements in loops

--- GhostServer/networkmanager.cpp
+++ GhostServer/networkmanager.cpp
@@ -3,6 +3,7 @@
 #include <memory>
 #include <chrono>
 #include <cstdlib>
+#include <queue>
 
 #include <SFML/Network.hpp>
 
@@ -103,8 +104,8 @@ void NetworkManager::StopServer()
         return;
     }
 
-    for (auto& client : this->clients) {
-        this->DisconnectPlayer(client);
+    while (this->clients.size() > 0) {
+        this->DisconnectPlayer(this->clients.back());
     }
 
     this->isRunning = false;
@@ -118,6 +119,7 @@ void NetworkManager::DisconnectPlayer(Client& c)
     sf::Packet packet;
     packet << HEADER::DISCONNECT << c.ID;
     int id = 0;
+    int toErase = -1;
     for (; id < this->clients.size(); ++id) {
         if (this->clients[id].IP != c.IP) {
             this->clients[id].tcpSocket->send(packet);
@@ -125,8 +127,12 @@ void NetworkManager::DisconnectPlayer(Client& c)
             emit this->OnNewEvent("Player " + QString::fromStdString(this->clients[id].name) + " has disconnected !");
             this->selector.remove(*this->clients[id].tcpSocket);
             this->clients[id].tcpSocket->disconnect();
-            this->clients.erase(this->clients.begin() + id);
+            toErase = id;
         }
+    }
+
+    if (toErase != -1) {
+        this->clients.erase(this->clients.begin() + toErase);
     }
 }
 
@@ -382,16 +388,23 @@ void NetworkManager::RunServer()
             if (this->selector.isReady(this->listener)) {
                 this->CheckConnection(); //A player wants to connect
             } else {
+                std::queue<Client*> toDisconnect;
+
                 for (int i = 0; i < this->clients.size(); ++i) {
                     if (this->selector.isReady(*this->clients[i].tcpSocket)) {
                         sf::Packet packet;
                         sf::Socket::Status status = this->clients[i].tcpSocket->receive(packet);
                         if (status == sf::Socket::Disconnected) {
-                            this->DisconnectPlayer(this->clients[i]);
+                            toDisconnect.push(&this->clients[i]);
                             continue;
                         }
                         this->TreatTCP(packet);
                     }
+                }
+
+                while (!toDisconnect.empty()) {
+                    this->DisconnectPlayer(*toDisconnect.front());
+                    toDisconnect.pop();
                 }
             }
         }
@@ -402,17 +415,28 @@ void NetworkManager::RunServer()
 
 void NetworkManager::DoHeartbeats()
 {
+    // We don't disconnect clients in the loop; else, the loop will have
+    // UB
+    std::queue<Client*> toDisconnect;
+
     for (auto& client : this->clients) {
         if (!client.returnedHeartbeat) {
             // Client didn't return heartbeat in time; sever connection
-            this->DisconnectPlayer(client);
+            toDisconnect.push(&client);
         } else {
             // Send a heartbeat
             client.heartbeatToken = rand();
             client.returnedHeartbeat = false;
             sf::Packet packet;
             packet << HEADER::HEART_BEAT << sf::Uint32(client.ID) << sf::Uint32(client.heartbeatToken);
-            client.tcpSocket->send(packet);
+            if (client.tcpSocket->send(packet) == sf::Socket::Disconnected) {
+                toDisconnect.push(&client);
+            }
         }
+    }
+
+    while (!toDisconnect.empty()) {
+        this->DisconnectPlayer(*toDisconnect.front());
+        toDisconnect.pop();
     }
 }
